893a675c0ce1d863c53347b13eb5639286f62f60
Jeff Vincent <jeff.d.vincent@gmail.com>
2019-09-23 09:38:03 -0700

    add Property viewset and URLs

--- drfmongo/urls.py
+++ drfmongo/urls.py
@@ -5,6 +5,7 @@ from drfmongo.api import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'property', views.PropertyViewSet)
 
 # Setup automatic URL routing
 # Additionally, we include login URLs for the browsable API.
